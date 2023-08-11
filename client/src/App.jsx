b76634afc605b270a8b6e153942bf0970c165aef
nachog86 <gomezignacio86@gmail.com>
2023-08-11 17:48:40 -0300

    cambios en mi rama local

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
-import Home from "./components/Home";
+import Home from "./components/Home/Home.jsx";
 import { Routes, Route } from "react-router-dom";
+import Landing from "./components/Landing/Landing";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Landing />} />
         <Route path="/home" element={<Home />} />
       </Routes>
     </>
