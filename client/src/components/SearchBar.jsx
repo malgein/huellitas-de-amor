2996bdca11bc5e7d2951f3fa84bb32d1f80367db
DESKTOP-BMQPELL\claudio <claudiolaguzzi@gmail.com>
2023-08-11 00:19:48 -0300

    seachbar

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,34 +1,29 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { getDogsByName } from '../../redux/actions';
-import { useHistory } from 'react-router-dom'; // Import useHistory hook
-
-
+import { useHistory } from 'react-router-dom';
 
 const SearchBar = () => {
   const dispatch = useDispatch();
-  const history = useHistory(); // Initialize useHistory hook
+  const history = useHistory();
   const [nombre, setNombre] = useState('');
 
   const handleInputChange = (event) => {
-    setNombre(event.target.value);
-  };
+    const newNombre = event.target.value;
+    setNombre(newNombre);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    dispatch(getDogsByName(nombre));
-    history.push('/byname'); 
+    dispatch(getDogsByName(newNombre));
+    history.push('/byname');
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form>
       <input
         type="text"
         placeholder="Search by name..."
         value={nombre}
         onChange={handleInputChange}
       />
-      <button type="submit" disabled={!nombre}>SEARCH</button>
     </form>
   );
 };
