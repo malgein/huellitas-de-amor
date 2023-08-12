1219edec776f348eb0d6b04343d014812694e08a
nachog86 <gomezignacio86@gmail.com>
2023-08-12 02:41:40 -0300

    revisar el filtrado del back, lo demas funciona todo en el server

--- server/src/Routes/index.js
+++ server/src/Routes/index.js
@@ -1,10 +1,17 @@
 const { Router } = require("express");
 const mascotas = require("./routesMascotas");
-const { getPetById, postPetById, getPets, getPetByName } = require("./controllers");
+const getPetById = require("../controllers/getPetById");
+const postPetById = require("../controllers/postPetById");
+const getPetByName = require("../controllers/getPetsByName");
+const filtradoMascotas = require("../controllers/filtradoMascotas");
+const getPets = require("../controllers/getPets");
 const router = Router();
 
 router.use("/mascotas", mascotas);
+router.get("/", getPets);
+router.get("/nombre", getPetByName);
 router.get("/:id", getPetById);
+router.get("/filtro", filtradoMascotas);
 router.post("/", postPetById);
 
 module.exports = router;
--- server/src/controllers/filtradoMascotas.js
+++ server/src/controllers/filtradoMascotas.js
@@ -1,25 +1,50 @@
-const express = require('express');
-const { mascota } = require('../models/Mascota'); 
-const router = express.Router();
+// const express = require('express');
+// const { mascota } = require('../models/Mascota'); 
+// const router = express.Router();
 
-router.get('/mascotas', async (req, res) => {
+// router.get('/filtro', async (req, res) => {
+//   try {
+//     const {  edad, sexo, tamano, raza } = req.query; 
+
+//     const query = {};
+   
+//     if (edad) query.edad = edad;
+//     if (sexo) query.sexo = sexo;
+//     if (tamano) query.tamano = tamano;
+//     if (raza) query.raza = raza; 
+
+    
+//     const mascotas = await mascota.findAll({ where: query });
+
+//     res.json(mascotas); // ver el json con los datos, tengamos en cuenta q esto hay q acomodarlo
+//   } catch (error) {
+//     res.status(500).send(error.message);// ver que le ponemos como mensaje
+//   }
+// });
+
+// module.exports = router; 
+const {mascota} = require('../models/Mascota');
+
+const filtradoMascotas = async (req, res) => {
   try {
-    const {  edad, sexo, tamano, raza } = req.query; 
+    const { edad, sexo, tamano, raza, especie, peso } = req.query;
 
     const query = {};
-   
+
     if (edad) query.edad = edad;
     if (sexo) query.sexo = sexo;
     if (tamano) query.tamano = tamano;
-    if (raza) query.raza = raza; 
+    if (raza) query.raza = raza;
+    if (especie) query.especie = especie;
+    if (peso) query.peso = peso;
 
-    
     const mascotas = await mascota.findAll({ where: query });
 
-    res.json(mascotas); // ver el json con los datos, tengamos en cuenta q esto hay q acomodarlo
-  } catch (error) {
-    res.status(500).send(error.message);// ver que le ponemos como mensaje
-  }
-});
+    res.json(mascotas);
 
-module.exports = router; 
+} catch (error) {
+  res.status(500).send(error.message);
+}
+};
+
+module.exports = filtradoMascotas;
