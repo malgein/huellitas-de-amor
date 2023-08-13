c85a34a745a84024532b631c92ce6707c09004f0
nachog86 <gomezignacio86@gmail.com>
2023-08-12 23:49:24 -0300

    Landing lista

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-
+import Landing from "./components/Landing/Landing";
 import Home from "./components/Home/Home";
 import { Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
@@ -12,10 +12,13 @@ import Detail from "./components/Detail/Detail";
 
 
 function App() {
+  
   return (
     <>
       <Nav />
       <Routes>
+      <Route path="/" element={<Landing />} />
+        
         <Route path="/home" element={<Home />} />
 
 
