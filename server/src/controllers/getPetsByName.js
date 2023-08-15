31afa0a5db20d35979ee39cb074daa1487d90680
Jossy Benitez <jossybr94@gmail.com>
2023-08-14 23:30:02 -0500

    Correciones base de datos

--- server/src/controllers/getPetsByName.js
+++ server/src/controllers/getPetsByName.js
@@ -6,16 +6,6 @@ const getPetsByName = async (nombre) => {
     let nombreMinuscula = nombre.toUpperCase();
     if (nombreMinuscula) {
       console.log(nombre);
-<<<<<<< HEAD
-      const petName = await Mascota.findOne({
-        where: { nombre: { [Op.iLike]: `%${nombre}%` } },
-      });
-      // return petName;
-      console.log("controlador", petName);
-    } else {
-      const mascotas = await Mascota.findAll();
-      return mascotas;
-=======
       // const petName = await Mascota.findOne({ where: 
       //   { nombre: { [Op.iLike]: `%${nombre}%` } } });
 
@@ -32,7 +22,6 @@ const getPetsByName = async (nombre) => {
       const mascotas = await Mascota.findAll();
     return mascotas;
     // return []
->>>>>>> 166efa192a3326f4ba07a814bc1fca40cd958c62
     }
   } catch (error) {
     throw { status: error?.status, message: error?.message };
