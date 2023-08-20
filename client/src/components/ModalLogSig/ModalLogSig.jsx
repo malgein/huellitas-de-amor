e0b3bb3159a213d1ba22f8a9b7ca873007f3e7b4
Jorge <116285852+JJtarazona@users.noreply.github.com>
2023-08-20 17:52:28 -0500

    Solucion de algunos errores + Eliminar Registro

--- client/src/components/ModalLogSig/ModalLogSig.jsx
+++ client/src/components/ModalLogSig/ModalLogSig.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { useAuth } from "../../context/AuthContext";
 
 import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/authContext";
 
 export default function ModalLogSig() {
   const [user, setUser] = useState({
