97fdefbaf61f877e57d507df40ff2c9a494a8300
Jossy Benitez <jossybr94@gmail.com>
2023-08-20 19:12:02 -0500

    Guardando componente registro

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ import PathRoutes from "./helpers/Routes.helper";
 import Footer from "./components/Footer/Footer";
 import { AuthProvider } from "./context/AuthContext";
 import ModalLogSig from "./components/ModalLogSig/ModalLogSig";
-import Registro from "./components/Registro/registro";
+import Registro from "./components/Registro/Registro";
 
 function App() {
   const location = useLocation();
