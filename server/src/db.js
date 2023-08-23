462e46489a77a7e3f9169d493c2a04714ceafa1c
OzielCortez <116935625+OzielCortez@users.noreply.github.com>
2023-08-23 17:54:37 -0600

    bdRailway

--- server/src/db.js
+++ server/src/db.js
@@ -7,7 +7,6 @@ const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(
   `postgresql://postgres:Awr2N2134BUaWYRvT9AW@containers-us-west-203.railway.app:5914/railway`,
-  ``,
   // `postgres://postgres:22511290@localhost/huellitas`,
 	{
 		logging: false, // set to console.log to see the raw SQL queries
