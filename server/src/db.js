a0399d2d73353070a13b73c4013c31f6a1e3372f
Thony <thonysilva1993@gmail.com>
2023-08-26 18:29:24 -0500

    eliminando modelos, rutas, y relaciones de rating innecesaria

--- server/src/db.js
+++ server/src/db.js
@@ -7,7 +7,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(
   // `postgresql://postgres:devZjxigFLUOiHZBcQxh@containers-us-west-127.railway.app:6739/railway`,
-  `postgres://postgres:22511290@localhost/huellitas`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/huellitas`,
 	{
 		logging: false, // set to console.log to see the raw SQL queries
 		native: false, // lets Sequelize know we can use pg-native for ~30% more speed
@@ -93,10 +93,10 @@ Comentario.hasMany(CasaDeAdopcion, {
   foreignKey: "comentarioId",
 }); */
 
-//!Ratings --> Casa de Adopciones
-CasaDeAdopcion.hasMany(Rating,{foreignKey: "ratingId"});
-Rating.belongsTo(CasaDeAdopcion, { foreignKey: "ratingId" });
-//!Ratings --> Casa de Adopciones
+// //!Ratings --> Casa de Adopciones
+// CasaDeAdopcion.hasMany(Rating,{foreignKey: "ratingId"});
+// Rating.belongsTo(CasaDeAdopcion, { foreignKey: "ratingId" });
+// //!Ratings --> Casa de Adopciones
 
 //Mascotas --> Casa de Adopciones
 Mascota.belongsTo(CasaDeAdopcion, {
