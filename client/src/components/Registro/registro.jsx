cdf12cf1abb4c2361f3ff57a779b8814edefd4bd
Jorge <116285852+JJtarazona@users.noreply.github.com>
2023-08-25 20:29:35 -0500

    solucion archivo registro

--- client/src/components/Registro/registro.jsx
+++ client/src/components/Registro/registro.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
-
-import styles from "./registro.module.css";
+import Swal from "sweetalert2";
+import styles from "./Registro.module.css";
 import FormInput from "../FormInput/FormInput";
 import { Button } from "@nextui-org/button";
 import { useNavigate } from "react-router-dom";
@@ -9,10 +9,9 @@ import { useNavigate } from "react-router-dom";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import validationSchema from "./Validaciones";
 import * as Yup from "yup";
-import ModalLogSig from "../ModalLogSig/ModalLogSig";
 
 const Registro = () => {
-  // const [registroExitoso, setRegistroExitoso] = useState(false);
+  //const [registroExitoso, setRegistroExitoso] = useState(false);
 
   const initialValues = {
     nombre: "",
@@ -30,100 +29,100 @@ const Registro = () => {
     axios
       .post("http://localhost:3001/usuario", values)
       .then((res) => {
-        // Manejar la respuesta del servidor, por ejemplo, mostrar un mensaje de éxito
-        alert("Usuario registrado con éxito");
-        setRegistroExitoso(true);
+        Swal.fire({
+          icon: "success",
+          title: "Registro exitoso",
+          text: "Usuario registrado con éxito",
+        });
 
-        // resetForm();
+        setRegistroExitoso(true);
       })
       .catch((err) => {
-        // Manejar errores, por ejemplo, mostrar un mensaje de error
-        alert("Hubo un error al registrar al usuario");
+        Swal.fire({
+          icon: "error",
+          title: "Error de registro",
+          text: "Hubo un error al registrar al usuario",
+        });
       });
   };
 
   return (
     <div className={styles.formcontainer}>
-      <div className="flex flex-col items-center">
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={onSubmit}
-          // enableReinitialize={true}
-        >
-          {({ isSubmitting, errors, values, setValues }) => (
-            <Form>
-              <div className={styles.tittle}>
-                <h1>Registrate</h1>
-              </div>
-              <div>
-                <FormInput label="Nombre" name="nombre" error={errors.nombre} />
-              </div>
-              <div>
-                <FormInput
-                  label="Apellido"
-                  name="apellido"
-                  error={errors.apellido}
-                />
-              </div>
-              <div>
-                <FormInput
-                  label="Nacionalidad"
-                  name="nacionalidad"
-                  error={errors.nacionalidad}
-                />
-              </div>
-              <div>
-                <FormInput
-                  label="Localizacion"
-                  name="localizacion"
-                  error={errors.localizacion}
-                />
-              </div>
-              <div>
-                <FormInput
-                  label="Direccion"
-                  name="direccion"
-                  error={errors.direccion}
-                />
-              </div>
-              <div>
-                <FormInput
-                  label="Telefono"
-                  name="telefono"
-                  error={errors.telefono}
-                />
-              </div>
-              <div>
-                <FormInput label="Acerca" name="acerca" error={errors.acerca} />
-              </div>
-              <div>
-                <FormInput label="Email" name="email" error={errors.email} />
-              </div>
-              <div>
-                <FormInput
-                  label="Contraseña"
-                  name="password"
-                  error={errors.password}
-                />
-              </div>
-              <Button
-                type="submit"
-                disabled={isSubmitting}
-                className="border border-black text-black hover:bg-slate-100 mt-8 bg-inherit "
-                size="lg"
-              >
-                Registrate
-              </Button>
-              {/* {registroExitoso && setValues(initialValues)} */}
-            </Form>
-          )}
-        </Formik>
-        <div className="m-4">
-          <h1 className="pb-4">------------ 0 ------------</h1>
-          <ModalLogSig />
-        </div>
-      </div>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+        // enableReinitialize={true}
+      >
+        {({ isSubmitting, errors, values, setValues }) => (
+          <Form>
+            <div className={styles.tittle}>
+              <h1>Registrate</h1>
+            </div>
+            <div>
+              <FormInput label="Nombre" name="nombre" error={errors.nombre} />
+            </div>
+            <div>
+              <FormInput
+                label="Apellido"
+                name="apellido"
+                error={errors.apellido}
+              />
+            </div>
+            <div>
+              <FormInput
+                label="Nacionalidad"
+                name="nacionalidad"
+                error={errors.nacionalidad}
+              />
+            </div>
+            <div>
+              <FormInput
+                label="Localizacion"
+                name="localizacion"
+                error={errors.localizacion}
+              />
+            </div>
+            <div>
+              <FormInput
+                label="Direccion"
+                name="direccion"
+                error={errors.direccion}
+              />
+            </div>
+            <div>
+              <FormInput
+                label="Telefono"
+                name="telefono"
+                error={errors.telefono}
+              />
+            </div>
+            <div>
+              <FormInput label="Acerca" name="acerca" error={errors.acerca} />
+            </div>
+            <div>
+              <FormInput label="Email" name="email" error={errors.email} />
+            </div>
+            <div>
+              <FormInput
+                label="Contraseña"
+                name="password"
+                error={errors.password}
+              />
+            </div>
+            <Button
+              type="submit"
+              disabled={isSubmitting}
+              className="border border-black text-black hover:bg-slate-100 mt-8 bg-inherit "
+              size="lg"
+            >
+              Registrate
+            </Button>
+            {/* {registroExitoso && setValues(initialValues)} */}
+          </Form>
+        )}
+      </Formik>
+      {/*  */}
     </div>
   );
 };
