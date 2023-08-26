1435ba1ff8c54dc31da5181e26156b427f7bbea6
Thony <thonysilva1993@gmail.com>
2023-08-26 18:14:02 -0500

    incorporacion ratings al modelo de CasaDeAdopcion, controlador y ruta

--- server/src/Routes/routesRateCasas.js
+++ server/src/Routes/routesRateCasas.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 const updateAverageRating = require("../controllers/updatedRateCasas");
-const newRateCasas = require("../controllers/newRateCasas");
+const newRateCasas = require("../controllers/postRatings");
 
 router.get("/:id", updateAverageRating);
 router.post("/", newRateCasas);
--- server/src/db.js
+++ server/src/db.js
@@ -6,8 +6,8 @@ const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 
 const sequelize = new Sequelize(
-  `postgresql://postgres:devZjxigFLUOiHZBcQxh@containers-us-west-127.railway.app:6739/railway`,
-  // `postgres://postgres:22511290@localhost/huellitas`,
+  // `postgresql://postgres:devZjxigFLUOiHZBcQxh@containers-us-west-127.railway.app:6739/railway`,
+  `postgres://postgres:22511290@localhost/huellitas`,
 	{
 		logging: false, // set to console.log to see the raw SQL queries
 		native: false, // lets Sequelize know we can use pg-native for ~30% more speed
