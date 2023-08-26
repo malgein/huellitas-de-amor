f596ebc1a184a60013b17658f5f74bfa1d0ea2f3
OzielCortez <116935625+OzielCortez@users.noreply.github.com>
2023-08-25 21:11:27 -0600

    serverDeploy

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
+require("dotenv").config();
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const fillHomes = require("./src/utils/fillHomes.js");
 const fillPets = require("./src/utils/fillPets.js");
-
+const PORT = process.env.PORT || 3001
 // Syncing all the models at once.
 conn.sync({
   force: false}).then(() => {
-  server.listen(3001, () => {
+  server.listen(PORT, () => {
     fillPets();
     // fillHomes()
     console.log("%s listening at 3001"); // eslint-disable-line no-console
