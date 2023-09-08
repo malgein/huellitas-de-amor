a0f6d155e475c640524edf19462ca219ccc524ac
Thony <thonysilva1993@gmail.com>
2023-09-08 10:09:13 -0500

    actualizacion 1000

--- server/src/Routes/routesUsuario.js
+++ server/src/Routes/routesUsuario.js
@@ -9,29 +9,29 @@ const getUsers = require("../controllers/getUsers");
 const modUserById = require("../controllers/modUsersById");
 const deleteUsersById = require("../controllers/deleteUsersById");
 const modCompleteUser = require("../controllers/modCompleteUser");
-const getUserById = require('../controllers/getUserById')
-const findTypesUsers = require('../controllers/findTypeUsers')
-const usuarioTipoController = require('../controllers/usuarioTipoController')
+const getUserById = require("../controllers/getUserById");
+const findTypesUsers = require("../controllers/findTypeUsers");
+const usuarioTipoController = require("../controllers/usuarioTipoController");
 
 router.post("/crearUsuario", async (req, res) => {
-  try {
-    const response = req.body;
-    const usuarioPost = await crearUsuario(response);
-    res.status(200).json(usuarioPost);
-  } catch (error) {
-    res.status(error.status || 500).json({ message: error.message });
-  }
+	try {
+		const response = req.body;
+		const usuarioPost = await crearUsuario(response);
+		res.status(200).json(usuarioPost);
+	} catch (error) {
+		res.status(error.status || 500).json({ message: error.message });
+	}
 });
 
-router.get('/tipoDeUsuario', async(req, res) => {
-  try{
-    // const {id} = req.params
-    const userType = await findTypesUsers()
-    return res.status(200).json(userType)
-  } catch(error){
-    return res.status(500).json({ mensaje: "Error en el servidor" });
-  }
-})
+router.get("/tipoDeUsuario", async (req, res) => {
+	try {
+		// const {id} = req.params
+		const userType = await findTypesUsers();
+		return res.status(200).json(userType);
+	} catch (error) {
+		return res.status(500).json({ mensaje: "Error en el servidor" });
+	}
+});
 
 //controlador que funciona con propositos de llenar la base de datos con datos de usuario para testeo
 // router.get("/fill", async (req, res) => {
@@ -61,59 +61,58 @@ router.get('/tipoDeUsuario', async(req, res) => {
 
 //Ruta que me trae todos los usuarios de la base de datos necesario para la gestion de usuarios en el componente admin
 router.get("/", async (req, res) => {
-  try {
-    const data = await getUsers();
-    console.log(data);
+	try {
+		const data = await getUsers();
+		console.log(data);
 
-//     return res.status(200).json(data);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
+		return res.status(200).json(data);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
 
 router.patch("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const response = req.body;
-    console.log(response);
-    const userId = await modUserById(id, response);
-    return res.status(200).json(userId);
-  } catch (error) {
-    return res.status(500).json({ mensaje: "Error en el servidor" });
-  }
+	try {
+		const { id } = req.params;
+		const response = req.body;
+		console.log(response);
+		const userId = await modUserById(id, response);
+		return res.status(200).json(userId);
+	} catch (error) {
+		return res.status(500).json({ mensaje: "Error en el servidor" });
+	}
 });
 
 router.delete("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const userId = await deleteUsersById(id);
-    return res.status(200).json(userId);
-  } catch (error) {
-    return res.status(500).json({ mensaje: "Error en el servidor" });
-  }
+	try {
+		const { id } = req.params;
+		const userId = await deleteUsersById(id);
+		return res.status(200).json(userId);
+	} catch (error) {
+		return res.status(500).json({ mensaje: "Error en el servidor" });
+	}
 });
 
 router.put("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const response = req.body;
-    console.log(response);
-    const userId = await modCompleteUser(id, response);
-    return res.status(200).json(userId);
-  } catch (error) {
-    return res.status(500).json({ mensaje: "Error en el servidor" });
-  }
+	try {
+		const { id } = req.params;
+		const response = req.body;
+		console.log(response);
+		const userId = await modCompleteUser(id, response);
+		return res.status(200).json(userId);
+	} catch (error) {
+		return res.status(500).json({ mensaje: "Error en el servidor" });
+	}
 });
 
-
 router.get("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const userId =  await getUserById(id);
-    return res.status(200).json(userId);
-  } catch (error) {
-    return res.status(500).json({ mensaje: "Error en el servidor" });
-  }
+	try {
+		const { id } = req.params;
+		const userId = await getUserById(id);
+		return res.status(200).json(userId);
+	} catch (error) {
+		return res.status(500).json({ mensaje: "Error en el servidor" });
+	}
 });
 
 //Probando obtener usuario por ID
@@ -130,8 +129,6 @@ router.get("/:id", async (req, res) => {
 //     return res.status(500).json({ mensaje: "Error en el servidor" });
 //   }
 // });
-
-
 
 // router.get('/relacionarTipo', async(req, res) => {
 //   try{
